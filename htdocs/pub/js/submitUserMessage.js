d64032c74774b1c2d5828a58c55f26940278e4de
nicolasblancom <nicolasblancom@gmail.com>
2023-06-12 13:02:12 +0200

    Append AI response to html

--- htdocs/pub/js/submitUserMessage.js
+++ htdocs/pub/js/submitUserMessage.js
@@ -10,8 +10,18 @@ form.addEventListener("submit", event => {
 
     messageInput.value = "";
     updateContextToAppend('user', messageText);
+    appendMessageToUi('user', messageText);
     sendMessage(contextAppend);
 });
+
+function appendMessageToUi(role, message) {
+    const messagesList = document.querySelector('.messages__list');
+    const htmlMarkup = `<li class="messages__item messages__item--${role}">
+        <span class="meessages__content">${message}</span>
+    </li>`;
+
+    messagesList.innerHTML += htmlMarkup;
+}
 
 function updateContextToAppend(role, message) {
     contextAppend.push({
@@ -26,18 +36,17 @@ function updateContextToAppend(role, message) {
 function sendMessage(contextAppend) {
     var formData = new FormData();
     formData.append('contextAppend', JSON.stringify(contextAppend));
-    
+
     fetch('/processMessage.php', {
         method: 'POST',
         body: formData
     })
-    .then(response => response.json())
-    .then(res => {
-        // TODO: remove console log
-        // console.log(res);
-        updateContextToAppend(res.role, res.content);
-        
-        // TODO: feedback en pantalla
-    })
-    .catch(err => console.log('Solicitud fallida', err));
+        .then(response => response.json())
+        .then(res => {
+            // TODO: remove console log
+            // console.log(res);
+            updateContextToAppend(res.role, res.content);
+            appendMessageToUi(res.role, res.content);
+        })
+        .catch(err => console.log('Solicitud fallida', err));
 }
